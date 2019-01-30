fe93ead4b9fdc90608845232fb86d1231cf611c5
PengtaoY <1197726544@qq.com>
2019-01-30 15:05:59 +0800

    添加控制台读取配置文件的程序

---
+++ 控制台读取配置文件/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace 控制台读取配置文件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //需要引用三个Nuget包
+            //注意将appsetting.json的编码格式设置为utf-8.然后属性里面设置始终复制
+
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var configuuation = builder.Build();
+
+            string sqlServer = configuuation["ConnectionStrings:SqlServer"];
+            Console.WriteLine($"配置文件中sqlServer的值：{ sqlServer}");
+
+            Console.ReadKey();
+        }
+    }
+}
