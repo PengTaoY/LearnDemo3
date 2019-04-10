85cc2d495bf399d8230017bdf9215afd9e949fd5
PengtaoY <1197726544@qq.com>
2019-04-10 13:59:50 +0800

    学习

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> valuePairs = new Dictionary<string, string>();
+
+            //valuePairs.Add("createuserid", "张三");
+            //valuePairs.Add("createdate", "李四");
+
+
+            //DateTime dateTime1 = new DateTime(2019, 3, 20, 8, 0, 0);
+            //DateTime dateTime2 = new DateTime(2019, 3, 20, 9, 0, 10);
+            //Console.WriteLine($"开始时间：{dateTime1.ToString()}     结束时间：{dateTime2.ToString()}"  );
+
+            //Console.WriteLine($"审批时长：{(int)(dateTime2.Subtract(dateTime1).Duration().TotalMinutes)} 秒。");  ;
+
+
+            //double s = 60;
+
+
+            Dictionary<int, bool> keyValuePairs = new Dictionary<int, bool>();
+            keyValuePairs.Add(1, true);
+            keyValuePairs.Add(1, false);
+            keyValuePairs.Add(2, true);
+            keyValuePairs.Add(2, true);
+
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine(item.Key + item.Value.ToString());
+            }
+
+            Console.WriteLine(TimeSpan.FromMinutes(60).ToString());
+
+
+
+
+
+            //List<Test> lsTest = new List<Test>();
+            //Test test1 = new Test { Id = 1, largeRegion = 1 };
+            //Test test2 = new Test { Id = 2, largeRegion = 2 };
+            //Test test3 = new Test { Id = 3, largeRegion = 3 };
+            //lsTest.Add(test1);
+            //lsTest.Add(test2);
+            //lsTest.Add(test3);
+
+            //Console.WriteLine("初始顺序");
+            //foreach (var item in lsTest)
+            //{
+            //    Console.WriteLine(item.Id + "  "+item.largeRegion);
+            //}
+
+            //Console.WriteLine("OrderBy以后的顺序");
+
+            //lsTest = lsTest.OrderBy(u => u.largeRegion).ToList();
+            //foreach (var item in lsTest)
+            //{
+            //    Console.WriteLine(item.Id + "  " + item.largeRegion);
+            //}
+
+
+            //Console.WriteLine("OrderByDescending以后的顺序");
+            //lsTest = lsTest.OrderByDescending(u => u.largeRegion).ToList();
+            //foreach (var item in lsTest)
+            //{
+            //    Console.WriteLine(item.Id + "  " + item.largeRegion);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Test
+    {
+        public int Id { get; set; }
+        public int largeRegion { get; set; }
+
+    }
+}
---
+++ async_await/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace async_await
+{
+    class Program
+    {
+        //https://www.cnblogs.com/liqingwen/p/5831951.html
+        private static readonly Stopwatch Watch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            //启动计时器
+            Watch.Start();
+
+            const string url1 = "http://www.cnblogs.com/";
+            const string url2 = "http://www.cnblogs.com/liqingwen/";
+
+            //两次调用 CountCharacters 方法(下载某网站内容，并统计字符的个数)
+            Task<int> t1 = CountCharacters(1, url1);
+            Task<int> t2 = CountCharacters(2, url2);
+
+            //三次调用ExtraOperation 方法(主要通过拼接字符串达到耗时操作)
+            for (int i = 0; i < 3; i++)
+            {
+                ExtraOperation(i + 1);
+            }
+
+            //控制台输出
+            Console.WriteLine($"{url1} 的字符个数：{t1.Result}");
+            Console.WriteLine($"{url2} 的字符个数：{t2.Result}");
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 统计字符个数
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        private static async Task<int> CountCharacters(int id, string address)
+        {
+            var wc = new WebClient();
+            Console.WriteLine($"开始调用 id = {id}: {Watch.ElapsedMilliseconds} ms");
+
+            var result = await wc.DownloadStringTaskAsync(address);
+            Console.WriteLine($"调用完成 id = {id}: {Watch.ElapsedMilliseconds} ms");
+
+            return result.Length;
+        }
+
+        /// <summary>
+        /// 额外操作
+        /// </summary>
+        /// <param name="v"></param>
+        private static void ExtraOperation(int id)
+        {
+            //拼接字符串进行耗时操作
+            var s = "";
+
+            for (int i = 0; i < 6000; i++)
+            {
+                s += i;
+            }
+            Console.WriteLine($"id = {id} 的ExtraOperation 方法完成：{Watch.ElapsedMilliseconds} ms");
+        }
+
+    }
+}
