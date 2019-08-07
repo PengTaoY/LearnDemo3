36321a37f488b62696bb19f2d178229383bd0f66
PengtaoY <1197726544@qq.com>
2019-08-07 09:38:15 +0800

    添加http页面解析方法

--- DateOperator/Program.cs
+++ DateOperator/Program.cs
@@ -9,20 +9,39 @@ namespace DateOperator
         {
 
 
-            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            //DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
 
-            DateTime startTime2 = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1),TimeZoneInfo.Local);
+            //DateTime startTime2 = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1),TimeZoneInfo.Local);
 
-            DateTime endTime = new DateTime(2019, 5, 1);
+            //DateTime endTime = new DateTime(2019, 5, 1);
 
-            StringBuilder sb = new StringBuilder();
+            //StringBuilder sb = new StringBuilder();
 
-            sb.Append("我爱北京天安门");
+            //sb.Append("我爱北京天安门");
+
+            //           "postTime": 1562205690000,
+            //"isTop": 0,
+            //"postTimeStr": "07/04 10:01",
+
+            //string sbs = sb.ToString().Substring(1);
+            //long timeStamp1 = (long)(endTime - startTime).TotalMilliseconds;
+            //long timeStamp2 = (long)(endTime - startTime2).TotalMilliseconds;
 
 
-            string sbs = sb.ToString().Substring(1);
-            long timeStamp1 = (long)(endTime - startTime).TotalMilliseconds;
-            long timeStamp2 = (long)(endTime - startTime2).TotalMilliseconds;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)); // 当地时区
+
+            DateTime dt = startTime.AddMilliseconds(1562205690000);
+
+
+
+            Console.WriteLine();
+
+
+
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ WebClientTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WebClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "www.baidu.com/123.html";
+
+            string s2 = s1.Replace(".html", "_all.html#p1");
+
+
+            string url = "我爱北京天安门";
+
+            var s = System.Web.HttpUtility.UrlEncode(url);
+
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
