7ab22b2809af114fd43c83f2eec9dae76b675fd7
PengtaoY <1197726544@qq.com>
2019-08-08 15:26:37 +0800

    截取两个标签（字符串）之间的内容

--- WebClientTest/Program.cs
+++ WebClientTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace WebClientTest
 {
@@ -16,16 +18,60 @@ namespace WebClientTest
 
             var s = System.Web.HttpUtility.UrlEncode(url);
 
+            string bbsUrl = "https://bbs.hupu.com";
+
             HttpItem httpItem = new HttpItem
             {
-                URL = "http://www.ething.net"
+                URL = bbsUrl + "/all-gambia"
             };
             HttpHelper httpHelper = new HttpHelper();
 
             var result = httpHelper.GetHtml(httpItem);
 
+            string pattern_between = "(?<=({0}))[.\\s\\S]*?(?=({1}))";
+
+            string pattern = string.Format(pattern_between, "<span class=\"textSpan\">", "</em>");
+
+            foreach (Match match in Regex.Matches(result.Html, pattern))
+            {
+                string res = match.Value;
+                string pattern_url = string.Format(pattern_between, "href=\"", "\"");
+                string pattern_title = string.Format(pattern_between, "title=\"", "\">");
+                string pattern_count = string.Format(pattern_between, "<em>", "回复");
+
+
+
+                string title = GetValueByPattern(match.Value, pattern_title);
+
+                string url_link = GetValueByPattern(match.Value, pattern_url);
+                string count = GetValueByPattern(match.Value, pattern_count).Trim();
+
+                Console.WriteLine($"标题：{title},链接地址：{bbsUrl + url_link}，{count}+回复");
+            }
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
             Console.WriteLine("Hello World!");
         }
+
+
+        static string GetValueByPattern(string content, string pattern)
+        {
+            MatchCollection m = Regex.Matches(content, pattern);
+
+            foreach (Match item in m)
+            {
+                return item.Value;
+            }
+            return "";
+        }
+
     }
 }
