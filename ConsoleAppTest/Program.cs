b307daf06a80e5ef873407ffce632cd7716c835e
PengtaoY <1197726544@qq.com>
2019-08-26 17:48:21 +0800

    测试代码

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -8,13 +8,66 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            List<OccupyCar> occupyCars = new List<OccupyCar>();
+            occupyCars.Add(new OccupyCar { car_id = 121, car_name = "张三", plate_num = "京123" });
+            occupyCars.Add(new OccupyCar { car_id = 122, car_name = "李四", plate_num = "京234" });
+
+            List<OccupyCar> occupyCars2 = new List<OccupyCar>();
+            occupyCars2.Add(new OccupyCar { car_id = 122, car_name = "李四", plate_num = "京234" });
+            occupyCars2.Add(new OccupyCar { car_id = 123, car_name = "王五", plate_num = "京456" });
+
+
+            IEnumerable<OccupyCar> union = occupyCars.Union(occupyCars2);
+
+            foreach (var item in union)
+            {
+                Console.WriteLine($"{item.car_id} {item.car_name} {item.plate_num}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            List<int> ls1 = new List<int>();
+            List<int> ls2 = new List<int>();
+            List<int> ls3 = new List<int>();
+
+            ls1.Add(1);
+            ls1.Add(2);
+
+
+            ls3.Add(2);
+            ls3.Add(3);
+
+
+            List<int> result = ls1.Union(ls2).Union(ls3).ToList();
+
+
+
+
+
+
+
+
+
+
+
+
             //Guid guid0 = new Guid("bde3e4a8-524b-4b63-a511-1daf6d6f192d");
             //Guid guid1 = new Guid("c0796d59-2ef3-483b-9582-5b547e7916c1");
             //Guid guid2 = new Guid("c0fb930c-326c-4f81-ad47-8af9dbe630a5");
 
             Guid guid33 = new Guid("C0FB930C-326C-4F81-AD47-8AQ96BE630A5");
 
-            Guid guid3 = new Guid("c0fb930c-326c-4f81-ad47-8aq96be630a5");
+            Guid guid3 = new Guid("c0fb930c-326c-4f81-ad47-8aa96be630a5");
 
             //Dictionary<string, string> valuePairs = new Dictionary<string, string>();
 
@@ -90,4 +143,37 @@ namespace ConsoleAppTest
         public int largeRegion { get; set; }
 
     }
+
+
+    public class OccupyCar : IEquatable<OccupyCar>
+    {
+        /// <summary>
+        /// 车辆ID
+        /// </summary>
+        public int car_id { get; set; }
+        /// <summary>
+        /// 车辆名称
+        /// </summary>
+        public string car_name { get; set; }
+        /// <summary>
+        /// 车牌号
+        /// </summary>
+        public string plate_num { get; set; }
+
+        public bool Equals(OccupyCar other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+            return this.car_id == other.car_id && this.car_name == other.car_name && this.plate_num == other.plate_num;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as OccupyCar);
+        public override int GetHashCode() => (car_id, car_name, plate_num).GetHashCode();
+
+    }
+
+
+
 }
---
+++ EFTest/Handler/SocketHandler.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EFTest.Handler
+{
+    public class SocketHandler
+    {
+        public const int BufferSize = 4096;
+        public string basestringjson = string.Empty;
+
+        WebSocket _socket;
+
+        SocketHandler(WebSocket socket)
+        {
+            _socket = socket;
+        }
+
+        async Task EchoLoop()
+        {
+            var buffer = new byte[BufferSize];
+            var seg = new ArraySegment<byte>(buffer);
+
+
+            while (_socket.State == WebSocketState.Open)
+            {
+                var incoming = await _socket.ReceiveAsync(seg, CancellationToken.None);
+                string receivemsg = Encoding.UTF8.GetString(buffer, 0, incoming.Count);
+                if (receivemsg == "get")
+                {
+                    string stringJson = "";
+
+                    List<AddressMatchProgressModel> infolist = new List<AddressMatchProgressModel>();
+                    Hashtable newtable = CommonInfo.ProgressInfo;
+                    if (newtable != null)
+                    {
+                        try
+                        {
+                            foreach (DictionaryEntry i in newtable)
+                            {
+                                AddressMatchProgressModel pgmodel = new AddressMatchProgressModel();
+                                pgmodel = (AddressMatchProgressModel)i.Value;
+                                infolist.Add(pgmodel);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    stringJson = JsonConvert.SerializeObject(infolist);
+
+
+                    string userMsg = stringJson;
+                    byte[] x = Encoding.UTF8.GetBytes(userMsg);
+                    var outgoing = new ArraySegment<byte>(x);
+                    await _socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                //var incoming = await _socket.ReceiveAsync(seg, CancellationToken.None);
+                //var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
+                //await _socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+
+            }
+        }
+
+        static async Task Acceptor(HttpContext hc, Func<Task> n)
+        {
+            if (!hc.WebSockets.IsWebSocketRequest)
+                return;
+
+            var socket = await hc.WebSockets.AcceptWebSocketAsync();
+            var h = new SocketHandler(socket);
+            await h.EchoLoop();
+        }
+
+        /// <summary>
+        /// branches the request pipeline for this SocketHandler usage
+        /// </summary>
+        /// <param name="app"></param>
+        public static void Map(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(SocketHandler.Acceptor);
+        }
+    }
+
+    internal class AddressMatchProgressModel
+    {
+    }
+}
