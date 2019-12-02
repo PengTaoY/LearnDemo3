ac4b7af7b5719c7e6ed7b2c3dd40f5fc72c0eb80
PengtaoY <1197726544@qq.com>
2019-12-02 13:19:41 +0800

    改进后的单例模式

--- 单例模式/Singleton.cs
+++ 单例模式/Singleton.cs
@@ -5,13 +5,22 @@ namespace 单例模式
     public class Singleton
     {
         private static Singleton _singleton = null;
+        private static object _singleton_Lock = new object(); //锁同步
         public static Singleton CreateInstance()
         {
             if (_singleton == null)
             {
-                Console.WriteLine("被创建！");
-                _singleton = new Singleton();
+                lock (_singleton_Lock)
+                {
+                    Console.WriteLine("路过");
+                    if (_singleton == null)
+                    {
+                        Console.WriteLine("被创建！");
+                        _singleton = new Singleton();
+                    }
+                }
             }
+
             return _singleton;
         }
     }
