2a29143afb4a18c5596395d6064243d87620b4c6
PengtaoY <1197726544@qq.com>
2019-12-02 13:38:52 +0800

    又添加了两个单例实现

---
+++ 单例模式/SingletonSecond.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 单例模式
+{
+    /*
+     利用静态构造函数实现单例模式
+        静态构造函数：只能有一个，无参数的，程序无法调用 。
+        同样是由CLR保证，在程序第一次使用该类之前被调用，而且只调用一次
+        同静态变量一样, 它会随着程序运行, 就被实例化, 同静态变量一个道理。
+         */
+    public class SingletonSecond
+    {
+        private static SingletonSecond _SingletonSecond = null;
+
+        static SingletonSecond()
+        {
+
+            _SingletonSecond = new SingletonSecond();
+        }
+
+        public static SingletonSecond CreateInstance()
+        {
+            return _SingletonSecond;
+        }
+    }
+}
---
+++ 单例模式/SingletonThird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 单例模式
+{
+    /*
+     * 利用静态变量实现单例模式
+     利用静态变量去实现单例,  由CLR保证，在程序第一次使用该类之前被调用，而且只调用一次
+    PS: 但是他的缺点也很明显, 在程序初始化后, 静态对象就被CLR构造了, 哪怕你没用。
+         */
+    public class SingletonThird
+    {
+        /// <summary>
+        /// 静态变量
+        /// </summary>
+        private static SingletonThird _singletonThird = new SingletonThird();
+
+        public static SingletonThird CreateInstance()
+        {
+            return _singletonThird;
+        }
+    }
+}
