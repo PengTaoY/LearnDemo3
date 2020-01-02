70d73686838acdcc7f492bd42e123e6157ed3f3f
PengtaoY <1197726544@qq.com>
2020-01-02 16:40:21 +0800

    添加一个HtmlAgilityPack测试项目

---
+++ HAPTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+
+namespace HAPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // From Web
+            var url = "https://bbs.hupu.com/all-gambia";
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+
+            var s = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'list')]");
+
+           var sss = s.First().InnerText;
+
+
+            var s2 = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'list')]").FindFirst("");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
