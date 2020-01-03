3ba37933af7577e9d6b9cea41fae75d9fc2478f6
PengtaoY <1197726544@qq.com>
2020-01-03 15:27:59 +0800

    添加测试项目
    添加Override的测试

---
+++ OverrideTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OverrideTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new WeatherInfo().ToString());
+
+            WeatherInfo weatherInfo = new WeatherInfo
+            {
+                City = "北京",
+                Temp = "朝阳",
+                Radar = "Dont know"
+            };
+
+            Console.WriteLine(weatherInfo.ToString());
+
+
+            Console.WriteLine(new WeatherInfo2().ToString());
+
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class WeatherInfo
+    {
+        public string City { get; set; } = "";
+        public string CityId { get; set; } = "";
+        public string Temp { get; set; } = "";
+        public string WD { get; set; } = "";
+        public string WS { get; set; } = "";
+        public string SD { get; set; } = "";
+        public string AP { get; set; } = "";
+        public string Njd { get; set; } = "";
+        public string WSE { get; set; } = "";
+        public string Time { get; set; } = "";
+        public string SM { get; set; } = "";
+        public string IsRadar { get; set; } = "";
+        public string Radar { get; set; } = "";
+
+        public override string ToString()
+        {
+            return string.Format($"City:{City}\r\n"
+                + $"CityId:{CityId}\r\n"
+                + $"Temp:{Temp}\r\n"
+                + $"WD:{WD}\r\n"
+                + $"WS:{WS}\r\n"
+                + $"SD:{SD}\r\n"
+                + $"AP:{AP}\r\n"
+                + $"Njd:{Njd}\r\n"
+                + $"WSD:{WSE}\r\n"
+                + $"Time:{Time}\r\n"
+                + $"SM:{SM}\r\n"
+                + $"IsRadar:{IsRadar}\r\n"
+                + $"Radar:{Radar}\r\n");
+        }
+    }
+
+    public class WeatherInfo2
+    {
+        public string City { get; set; } = "";
+
+        public string Temp { get; set; } = "";
+    }
+}
---
+++ UnitTestProject1/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+
+    }
+}
